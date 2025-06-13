0c32105d4eff03e7d526fc8f357dce7fdbbde4f1
clod10 <claudia.angileri@gmail.com>
2025-06-13 16:49:13 +0200

    set up

--- sketch.js
+++ sketch.js
@@ -1,3 +1,23 @@
+let trame = [];
+let urbane = [];
+function preload() {
+  trame = [
+    loadImage("./assets/lettere/trame/t.svg"),
+    loadImage("./assets/lettere/trame/r.svg"),
+    loadImage("./assets/lettere/trame/a.svg"),
+    loadImage("./assets/lettere/trame/m.svg"),
+    loadImage("./assets/lettere/trame/e.svg"),
+  ];
+  urbane = [
+    loadImage("./assets/lettere/trame/u.svg"),
+    loadImage("./assets/lettere/trame/r.svg"),
+    loadImage("./assets/lettere/trame/b.svg"),
+    loadImage("./assets/lettere/trame/a.svg"),
+    loadImage("./assets/lettere/trame/n.svg"),
+    loadImage("./assets/lettere/trame/e.svg"),
+  ];
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
